f5e15c02646b1ee11a53c62a3f5a06b13e55a8c7
vladmeh <vladmeh@gmail.com>
2022-04-16 00:21:17 +0300

    ch05_01. Core.

---
+++ src/core_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestPut(t *testing.T) {
+	const key = "create-key"
+	const value = "create-value"
+
+	var val interface{}
+	var contains bool
+
+	defer delete(store, key)
+
+	_, contains = store[key]
+
+	if contains {
+		t.Error("key/value already exists")
+	}
+
+	err := Put(key, value)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	val, contains = store[key]
+	if !contains {
+		t.Error("create failed")
+	}
+
+	if val != value {
+		t.Error("val/value mismatch")
+	}
+}
+
+func TestGet(t *testing.T) {
+	const key = "read-key"
+	const value = "read-value"
+
+	var val interface{}
+	var err error
+
+	defer delete(store, key)
+
+	val, err = Get(key)
+	if err == nil {
+		t.Error("expected an error")
+	}
+	if !errors.Is(err, ErrorNoSuchKey) {
+		t.Error("unexpected error: ", err)
+	}
+
+	store[key] = value
+
+	val, err = Get(key)
+	if err != nil {
+		t.Error("unexpected error: ", err)
+	}
+
+	if val != value {
+		t.Error("val/value mismatch")
+	}
+}
+
+func TestDelete(t *testing.T) {
+	const key = "delete-key"
+	const value = "delete-value"
+
+	var contains bool
+
+	defer delete(store, key)
+
+	store[key] = value
+
+	_, contains = store[key]
+	if !contains {
+		t.Error("key/value doesn't exist")
+	}
+
+	_ = Delete(key)
+
+	_, contains = store[key]
+	if contains {
+		t.Error("Delete failed")
+	}
+}
