a34917a46f9ea3045279100db571d4c5c40fbab2
vladmeh <vladmeh@gmail.com>
2022-04-30 00:10:31 +0300

    ch05_07. Docker build kvs.

---
+++ file_transact.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/url"
+	"os"
+	"sync"
+)
+
+type FileTransactionLogger struct {
+	events       chan<- Event // Канал только для записи, для передачи событий
+	errors       <-chan error // Канал только для чтения, для приема ошибок
+	lastSequence uint64       // Последний использованный порядковый номер
+	file         *os.File     // Местоположение файла журнала
+	wg           *sync.WaitGroup
+}
+
+func (l *FileTransactionLogger) WritePut(key, value string) {
+	l.wg.Add(1)
+	l.events <- Event{EventType: EventPut, Key: key, Value: value}
+}
+
+func (l *FileTransactionLogger) WriteDelete(key string) {
+	l.wg.Add(1)
+	l.events <- Event{EventType: EventDelete, Key: key}
+}
+
+func (l *FileTransactionLogger) Err() <-chan error {
+	return l.errors
+}
+
+func (l *FileTransactionLogger) Run() {
+	events := make(chan Event, 16) // Создать канал событий
+	l.events = events
+
+	errors := make(chan error, 1) // Создать канал ошибок
+	l.errors = errors
+
+	go func() {
+		for e := range events { // Извлечь следующее событие Event
+			l.lastSequence++ // Увеличить порядковый номер
+
+			e.Key = url.QueryEscape(e.Key)
+			e.Value = url.QueryEscape(e.Value)
+
+			_, err := fmt.Fprintf( // Записать событие в журнал
+				l.file,
+				"%d\t%d\t%s\t%s\n",
+				l.lastSequence, e.EventType, e.Key, e.Value)
+
+			if err != nil {
+				errors <- err
+				return
+			}
+
+			l.wg.Done()
+		}
+	}()
+}
+
+func (l *FileTransactionLogger) ReadEvents() (<-chan Event, <-chan error) {
+	scanner := bufio.NewScanner(l.file) // Создать Scanner для чтения l.file
+	outEvent := make(chan Event)        // Не буферизованный канал событий
+	outError := make(chan error, 1)     // Буферизованный канал ошибок
+
+	go func() {
+		var e Event
+
+		defer close(outEvent) // Закрыть каналы
+		defer close(outError) // по завершении сопрограммы
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if _, err := fmt.Sscanf(line, "%d\t%d\t%s\t%s",
+				&e.Sequence, &e.EventType, &e.Key, &e.Value); err != nil {
+				outError <- fmt.Errorf("input parse error: %w", err)
+				return
+			}
+
+			// Проверка целостности!
+			// Порядковые номера последовательно увеличиваются?
+			if l.lastSequence >= e.Sequence {
+				outError <- fmt.Errorf("transaction numbers out of ssequence")
+				return
+			}
+
+			uk, err := url.QueryUnescape(e.Key)
+			if err != nil {
+				outError <- fmt.Errorf("key decoding failure: %w", err)
+				return
+			}
+
+			uv, err := url.QueryUnescape(e.Value)
+			if err != nil {
+				outError <- fmt.Errorf("value decoding failure: %w", err)
+				return
+			}
+
+			e.Key = uk
+			e.Value = uv
+			l.lastSequence = e.Sequence // Запомнить последний использованный порядковый номер
+
+			outEvent <- e // Отправить событие along
+		}
+
+		if err := scanner.Err(); err != nil {
+			outError <- fmt.Errorf("transaction log read failure: %w", err)
+			return
+		}
+	}()
+
+	return outEvent, outError
+}
+
+func NewFileTransactionLogger(filename string) (TransactionLogger, error) {
+	var err error
+	var l = FileTransactionLogger{wg: &sync.WaitGroup{}}
+
+	l.file, err = os.OpenFile(filename, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open transaction log file: %w", err)
+	}
+
+	return &l, nil
+}
+
+func (l *FileTransactionLogger) Wait() {
+	l.wg.Wait()
+}
+
+func (l *FileTransactionLogger) Close() error {
+	l.wg.Wait()
+
+	if l.events != nil {
+		close(l.events)
+	}
+
+	return l.file.Close()
+}
+
+func (l *FileTransactionLogger) LastSequence() uint64 {
+	return l.lastSequence
+}
---
+++ service.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var logger TransactionLogger
+
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Получить ключ из запроса
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body) // Тело запроса хранит значение
+	defer r.Body.Close()
+
+	if err != nil { // Если возникла ошибка, сообщить о ней
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	err = Put(key, string(value)) // Сохранить значение как строку
+	if err != nil {               // Если возникла ошибка, сообщить о ней
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	logger.WritePut(key, string(value))
+
+	w.WriteHeader(http.StatusCreated) // Все хорошо! Вернуть статус 201
+
+	log.Printf("PUT key=%s value=%s\n", key, string(value))
+}
+
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Получить ключ из запроса
+	key := vars["key"]
+
+	value, err := Get(key) // Получить значение для данного ключа
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(value)) // Записать значение в ответ
+
+	log.Printf("GET key=%s\n", key)
+}
+
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Получить ключ из запроса
+	key := vars["key"]
+
+	err := Delete(key)
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	logger.WriteDelete(key)
+
+	log.Printf("DELETE key=%s\n", key)
+}
+
+func initializeTransactionLog() error {
+	var err error
+
+	logger, err = NewFileTransactionLogger("transaction.log")
+	if err != nil {
+		return fmt.Errorf("failed to create event logger: %w", err)
+	}
+
+	eventsCh, errorsCh := logger.ReadEvents()
+	e, ok := Event{}, true
+
+	for ok && err == nil {
+		select {
+		case err, ok = <-errorsCh: // Получает ошибки
+		case e, ok = <-eventsCh:
+			switch e.EventType {
+			case EventDelete: // Получено событие DELETE
+				err = Delete(e.Key)
+			case EventPut: // Получено событие PUT
+				err = Put(e.Key, e.Value)
+			}
+		}
+	}
+
+	logger.Run()
+
+	return err
+}
+
+func main() {
+	err := initializeTransactionLog()
+	if err != nil {
+		panic(err)
+	}
+
+	r := mux.NewRouter()
+
+	// Зарегистрировать обработчики HTTP-запросов
+	// в которых указан путь "/v1/{key}"
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
